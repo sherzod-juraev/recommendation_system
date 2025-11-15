f63adb9b175a7124539e690ef462419bf1beeebd
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-16 00:45:08 +0500

    genre_of_interest package updated in brain package of modules package

---
+++ modules/brain/genre_of_interest/recommend.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, distinct
+from uuid import UUID
+from .select_from_db import get_interest
+from ...learn.user_interests import UserInterest
+from ...recommendation.books import Book
+from ...recommendation.genres import Genre
+
+
+async def conclusion(
+        db: AsyncSession,
+        user_id: UUID,
+        /
+) -> list[Book]:
+    genres_id_list = await get_interest(db, user_id)
+    query = select(distinct(UserInterest.book_id)).join(
+        UserInterest.book, UserInterest.book_id == Book.id
+    ).join(
+        Book.book_genres
+    ).where(
+        UserInterest.user_id != user_id,
+        UserInterest.degree >= 3
+    ).where(
+        Genre.id.in_(genres_id_list)
+    ).order_by(UserInterest.created_at.desc()).limit(10)
---
+++ modules/brain/genre_of_interest/select_from_db.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from ...learn.user_interests import UserInterest
+from uuid import UUID
+
+
+async def get_interest(
+        db: AsyncSession,
+        user_id: UUID,
+        /
+) -> list[str]:
+    query = select(UserInterest).where(UserInterest.user_id == user_id).order_by(UserInterest.created_at.desc()).limit(20)
+    result = await db.execute(query)
+    user_interests = result.scalars().all()
+    length = len(user_interests)
+    genres_id_set = {}
+    for i in range(length):
+        genre_length = len(user_interests.book.book_genres)
+        for j in range(genre_length):
+            genres_id_set.add(user_interests.book.book_genres[j].id)
+    return list(genres_id_set)
