2e72f686176a06050a52f2c00f52aa3cc7ceca86
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-16 11:04:19 +0500

    response package updated in brain package of modules package

--- modules/brain/__init__.py
+++ modules/brain/__init__.py
@@ -1,1 +1,11 @@
-from .genre_of_interest import conclusion as genre_conclusion
+from fastapi import APIRouter
+from .response import response_router
+
+brain_router = APIRouter()
+
+
+brain_router.include_router(
+    response_router,
+    prefix='/response',
+    tags=['Response']
+)
---
+++ modules/brain/response/__init__.py
@@ -0,0 +1,1 @@
+from .router import response_router
